6103ece6a025f47e9f4975e0049888fa118d6af8
Julio Cesar Polmann <juliocesarpolmann@gmail.com>
2023-04-10 17:26:55 -0300

    First

--- github-api-tutorial-main/app.js
+++ github-api-tutorial-main/app.js
@@ -9,12 +9,14 @@ gitHubForm.addEventListener('submit', (e) => {
 
     // Get the GitHub username input field on the DOM
     let usernameInput = document.getElementById('usernameInput');
+    let repoInput = document.getElementById('repoInput');
 
     // Get the value of the GitHub username input field
     let gitHubUsername = usernameInput.value;
+    let gitHubRepo = repoInput.value;
 
     // Run GitHub API function, passing in the GitHub username
-    requestUserRepos(gitHubUsername)
+    requestUserRepos(gitHubUsername, gitHubRepo)
         .then(response => response.json()) // parse response into json
         .then(data => {
             // update html with data from github
@@ -22,43 +24,54 @@ gitHubForm.addEventListener('submit', (e) => {
                 // Get the ul with id of userRepos
 
                 if (data.message === "Not Found") {
-                    let ul = document.getElementById('userRepos');
-
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
-
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`);
-                    // Append each li to the ul
-                    ul.appendChild(li);
+                    let alertContainer = document.getElementById('alertContainer');
+                    alertContainer.innerHTML = (`
+                    <div class="alert alert-warning alert-dismissible" role="alert">
+                        <strong>Catapimbas!</strong> Esse usuário ou repositório não existe.
+                    </div>
+                    `);
+                    alertContainer.classList.remove('d-none')
+                    let tableBody = document.getElementById('commitData');
+                    tableBody.innerHTML = ''
                 } else {
+                    let alertContainer = document.getElementById('alertContainer');
+                    alertContainer.classList.add('d-none')
 
-                    let ul = document.getElementById('userRepos');
+            //         let ul = document.getElementById('userRepos');
 
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
+            //         // Create variable that will create li's to be added to ul
+            //         let li = document.createElement('li');
 
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
+            //         // Add Bootstrap list item class to each li
+            //         li.classList.add('list-group-item')
 
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
-                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-            `);
+            //         // Create the html markup for each li
+            //         li.innerHTML = (`
+            //     <p><strong>Repo:</strong> ${data[i].commit.author.name}</p>
+            //     <p><strong>Description:</strong> ${data[i].commit.author.date}</p>
+            //     <p><strong>Description:</strong> ${data[i].commit.message}</p>
+            //     <p><strong>URL:</strong> <a href="${data[i].commit.url}">${data[i].commit.url}</a></p>
+            // `);
+                    let tableBody = document.getElementById('commitData');
+
+                    let tableRow = document.createElement('tr');
+                    tableRow.innerHTML = (`
+                    <td>${data[i].commit.author.date}</td>
+                    <td>${data[i].commit.author.name}</td>
+                    <td>${data[i].commit.message}</td>
+                    <td><a href="${data[i].commit.url}">${data[i].commit.url}</a></td>
+                    `);
+
+                    tableBody.appendChild(tableRow);
 
                     // Append each li to the ul
-                    ul.appendChild(li);
+                    // ul.appendChild(li);
                 }
             }
         })
 })
 
-function requestUserRepos(username) {
+function requestUserRepos(username, repos) {
     // create a variable to hold the `Promise` returned from `fetch`
-    return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
+    return Promise.resolve(fetch(`https://api.github.com/repos/${username}/${repos}/commits`));
 }
